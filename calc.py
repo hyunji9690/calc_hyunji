5eee9075dd8b56b8548bbd39f6fb263c5aeeab15
leeyeanju <yeanju0912@naver.com>
2023-04-11 21:19:44 +0900

    modified code

--- calc.py
+++ calc.py
@@ -2,6 +2,12 @@
 # return sum
 def plus(x, y):
     return x+y
+def minus(x, y):
+    return x-y
+def multiplication(x, y):
+    return x*y
+def division(x, y):
+    return x/y
 
 # main function
 def main():
@@ -15,7 +21,11 @@ def main():
             x = int(input())
             print("Second Number")
             y = int(input())
-            print("answer : ", plus(x,y))
+            print("answer :")
+            print("plus = ", plus(x, y))
+            print("minus = ", minus(x, y))
+            print("multiplication = ", multiplication(x, y))
+            print("division = ", division(x, y))
         elif check > 1:
             print("Unsupported")
         else:
